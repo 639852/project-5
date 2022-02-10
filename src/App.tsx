dcfcd797b35ea1d4a28216e520b7d79d393a0123
639852 <sasha.bobyliev@mail.ru>
2022-02-10 13:43:30 +0400

    Add apiKey

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,12 @@ import './App.css';
 import { getTickets } from './store/slices/ticketSlice';
 
 initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyBCNrarb669AGcrmDiZZBVqJ97UizIBrXw',
+  authDomain: 'dashboard-c6de9.firebaseapp.com',
+  projectId: 'dashboard-c6de9',
+  storageBucket: 'dashboard-c6de9.appspot.com',
+  messagingSenderId: '948469530326',
+  appId: '1:948469530326:web:954265409de1c3ee665377',
 });
 
 function App() {
